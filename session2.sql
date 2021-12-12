e2fddab67c6875eeb40cf16dd71dada21443f864
Sametyorulmaz <sametyorulmaz777@gmail.com>
2021-12-13 00:16:05 +0300

    sql2 was composed.

--- session2.sql
+++ session2.sql
@@ -105,10 +105,21 @@
 	
 	/* customers tablosunda, isimleri (FirstName) üç harfli olan müşterilerin FirstName, 
 	LastName ve City bilgilerini	listeleyen sorguyu yazınız*/
+
+	SELECT  FirstName,LastName,City
+	FROM customers
+	WHERE FirstName like  '___';
+
 	
 	
 	/* customers tablosunda, soyisimleri Sch veya Go ile başlayan müşterilerin FirstName, 
 	LastName ve City bilgilerini listeleyen sorguyu yazınız*/
+	
+	
+	SELECT  FirstName,LastName,City
+	FROM customers
+	WHERE LastName like 'sch%';
+	
 	
 	
 	
